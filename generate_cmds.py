7c6bc86245c3771c47057b3c3f4cdb9eaace9a27
Mateo de Mayo <mateo.demayo@tum.de>
2024-11-04 12:13:37 +0100

    Add generate_cmds.py

---
+++ generate_cmds.py
@@ -0,0 +1,57 @@
+from pathlib import Path
+
+paths = [Path("$msd/M_monado_datasets/MG_reverb_g2/MGO_others/MGO01_low_light.zip"), Path("$msd/M_monado_datasets/MG_reverb_g2/MGO_others/MGO02_hand_puncher.zip"), Path("$msd/M_monado_datasets/MG_reverb_g2/MGO_others/MGO03_hand_shooter_easy.zip"), Path("$msd/M_monado_datasets/MG_reverb_g2/MGO_others/MGO04_hand_shooter_hard.zip"), Path("$msd/M_monado_datasets/MG_reverb_g2/MGO_others/MGO05_inspect_easy.zip"), Path("$msd/M_monado_datasets/MG_reverb_g2/MGO_others/MGO06_inspect_hard.zip"), Path("$msd/M_monado_datasets/MG_reverb_g2/MGO_others/MGO07_mapping_easy.zip"), Path("$msd/M_monado_datasets/MG_reverb_g2/MGO_others/MGO08_mapping_hard.zip"), Path("$msd/M_monado_datasets/MG_reverb_g2/MGO_others/MGO09_short_1_updown.zip"), Path("$msd/M_monado_datasets/MG_reverb_g2/MGO_others/MGO10_short_2_panorama.zip"), Path("$msd/M_monado_datasets/MG_reverb_g2/MGO_others/MGO11_short_3_backandforth.zip"), Path("$msd/M_monado_datasets/MG_reverb_g2/MGO_others/MGO12_freemovement_long_session.zip"), Path("$msd/M_monado_datasets/MG_reverb_g2/MGO_others/MGO13_sudden_movements.zip"), Path("$msd/M_monado_datasets/MG_reverb_g2/MGO_others/MGO14_flickering_light.zip"), Path("$msd/M_monado_datasets/MG_reverb_g2/MGO_others/MGO15_seated_screen.zip"), Path("$msd/M_monado_datasets/MI_valve_index/MIO_others/MIO01_hand_puncher_1.zip"), Path("$msd/M_monado_datasets/MI_valve_index/MIO_others/MIO02_hand_puncher_2.zip"), Path("$msd/M_monado_datasets/MI_valve_index/MIO_others/MIO03_hand_shooter_easy.zip"), Path("$msd/M_monado_datasets/MI_valve_index/MIO_others/MIO04_hand_shooter_hard.zip"), Path("$msd/M_monado_datasets/MI_valve_index/MIO_others/MIO05_inspect_easy.zip"), Path("$msd/M_monado_datasets/MI_valve_index/MIO_others/MIO06_inspect_hard.zip"), Path("$msd/M_monado_datasets/MI_valve_index/MIO_others/MIO07_mapping_easy.zip"), Path("$msd/M_monado_datasets/MI_valve_index/MIO_others/MIO08_mapping_hard.zip"), Path("$msd/M_monado_datasets/MI_valve_index/MIO_others/MIO09_short_1_updown.zip"), Path("$msd/M_monado_datasets/MI_valve_index/MIO_others/MIO10_short_2_panorama.zip"), Path("$msd/M_monado_datasets/MI_valve_index/MIO_others/MIO11_short_3_backandforth.zip"), Path("$msd/M_monado_datasets/MI_valve_index/MIO_others/MIO12_moving_screens.zip"), Path("$msd/M_monado_datasets/MI_valve_index/MIO_others/MIO13_moving_person.zip"), Path("$msd/M_monado_datasets/MI_valve_index/MIO_others/MIO14_moving_props.zip"), Path("$msd/M_monado_datasets/MI_valve_index/MIO_others/MIO15_moving_person_props.zip"), Path("$msd/M_monado_datasets/MI_valve_index/MIO_others/MIO16_moving_screens_person_props.zip"), Path("$msd/M_monado_datasets/MI_valve_index/MIP_playing/MIPB_beat_saber/MIPB01_beatsaber_100bills_360_normal.zip"), Path("$msd/M_monado_datasets/MI_valve_index/MIP_playing/MIPB_beat_saber/MIPB02_beatsaber_crabrave_360_hard.zip"), Path("$msd/M_monado_datasets/MI_valve_index/MIP_playing/MIPB_beat_saber/MIPB03_beatsaber_countryrounds_360_expert.zip"), Path("$msd/M_monado_datasets/MI_valve_index/MIP_playing/MIPB_beat_saber/MIPB04_beatsaber_fitbeat_hard.zip"), Path("$msd/M_monado_datasets/MI_valve_index/MIP_playing/MIPB_beat_saber/MIPB05_beatsaber_fitbeat_360_expert.zip"), Path("$msd/M_monado_datasets/MI_valve_index/MIP_playing/MIPB_beat_saber/MIPB06_beatsaber_fitbeat_expertplus_1.zip"), Path("$msd/M_monado_datasets/MI_valve_index/MIP_playing/MIPB_beat_saber/MIPB07_beatsaber_fitbeat_expertplus_2.zip"), Path("$msd/M_monado_datasets/MI_valve_index/MIP_playing/MIPB_beat_saber/MIPB08_beatsaber_long_session_1.zip"), Path("$msd/M_monado_datasets/MI_valve_index/MIP_playing/MIPP_pistol_whip/MIPP01_pistolwhip_blackmagic_hard.zip"), Path("$msd/M_monado_datasets/MI_valve_index/MIP_playing/MIPP_pistol_whip/MIPP02_pistolwhip_lilith_hard.zip"), Path("$msd/M_monado_datasets/MI_valve_index/MIP_playing/MIPP_pistol_whip/MIPP03_pistolwhip_requiem_hard.zip"), Path("$msd/M_monado_datasets/MI_valve_index/MIP_playing/MIPP_pistol_whip/MIPP04_pistolwhip_revelations_hard.zip"), Path("$msd/M_monado_datasets/MI_valve_index/MIP_playing/MIPP_pistol_whip/MIPP05_pistolwhip_thefall_hard_2pistols.zip"), Path("$msd/M_monado_datasets/MI_valve_index/MIP_playing/MIPP_pistol_whip/MIPP06_pistolwhip_thegrave_hard.zip"), Path("$msd/M_monado_datasets/MI_valve_index/MIP_playing/MIPT_thrill_of_the_fight/MIPT01_thrillofthefight_setup.zip"), Path("$msd/M_monado_datasets/MI_valve_index/MIP_playing/MIPT_thrill_of_the_fight/MIPT02_thrillofthefight_fight_1.zip"), Path("$msd/M_monado_datasets/MI_valve_index/MIP_playing/MIPT_thrill_of_the_fight/MIPT03_thrillofthefight_fight_2.zip"), Path("$msd/M_monado_datasets/MO_odyssey_plus/MOO_others/MOO01_hand_puncher_1.zip"), Path("$msd/M_monado_datasets/MO_odyssey_plus/MOO_others/MOO02_hand_puncher_2.zip"), Path("$msd/M_monado_datasets/MO_odyssey_plus/MOO_others/MOO03_hand_shooter_easy.zip"), Path("$msd/M_monado_datasets/MO_odyssey_plus/MOO_others/MOO04_hand_shooter_hard.zip"), Path("$msd/M_monado_datasets/MO_odyssey_plus/MOO_others/MOO05_inspect_easy.zip"), Path("$msd/M_monado_datasets/MO_odyssey_plus/MOO_others/MOO06_inspect_hard.zip"), Path("$msd/M_monado_datasets/MO_odyssey_plus/MOO_others/MOO07_mapping_easy.zip"), Path("$msd/M_monado_datasets/MO_odyssey_plus/MOO_others/MOO08_mapping_hard.zip"), Path("$msd/M_monado_datasets/MO_odyssey_plus/MOO_others/MOO09_short_1_updown.zip"), Path("$msd/M_monado_datasets/MO_odyssey_plus/MOO_others/MOO10_short_2_panorama.zip"), Path("$msd/M_monado_datasets/MO_odyssey_plus/MOO_others/MOO11_short_3_backandforth.zip"), Path("$msd/M_monado_datasets/MO_odyssey_plus/MOO_others/MOO12_freemovement_long_session.zip"), Path("$msd/M_monado_datasets/MO_odyssey_plus/MOO_others/MOO13_sudden_movements.zip"), Path("$msd/M_monado_datasets/MO_odyssey_plus/MOO_others/MOO14_flickering_light.zip"), Path("$msd/M_monado_datasets/MO_odyssey_plus/MOO_others/MOO15_seated_screen.zip"), Path("$msd/M_monado_datasets/MO_odyssey_plus/MOO_others/MOO16_still.zip")]  # fmt: skip
+
+for zip_path in paths:
+    ds_name = zip_path.stem
+    ds_code = ds_name.split("_")[0]
+
+    if ds_code.startswith("MG"):
+        ds_calib = "msdmg_calib"
+    elif ds_code.startswith("MO"):
+        ds_calib = "msdmo_calib"
+    elif ds_code.startswith("MI"):
+        ds_calib = "msdmi_calib"
+    else:
+        raise ValueError(f"Unknown dataset code: {ds_code}")
+
+    print(
+        f"""
+while not test -e {ds_name}_exists
+    sleep 1
+    echo "waiting for {ds_name}_exists"
+end"""
+    )
+
+    print(
+        f"mkdir -p results/{ds_code}; rm -rf results/{ds_code}/*; mkdir -p results/{ds_code}/input"
+    )
+
+    print(
+        f"$xrtmet/euroc/dmvio_ops.py euroc2dm_files {ds_name} results/{ds_code}/input"
+    )
+    print(
+        f"build/bin/dmvio_dataset useimu=1 mode=1 use16Bit=0 preset=0 nogui=0 nolog=1 quiet=1 start=10 \\"
+    )
+    print(f"  files={ds_name}/mav0/cam0/data \\")
+    print(f"  imuFile=results/{ds_code}/input/imu.txt \\")
+    print(f"  gtFile={ds_name}/mav0/gt/data.csv \\")
+    print(f"  tsFile=results/{ds_code}/input/times.txt \\")
+    print(f"  calib=configs/{ds_calib}/cam0.txt \\")
+    print(f"  imuCalib=configs/{ds_calib}/imuCalib.yaml \\")
+    print(f"  settingsFile=./configs/tumvi.yaml \\")
+    print(f"  resultsPrefix=results/{ds_code}/ \\")
+    print(f" > /dev/null")
+
+    print(f"rm {ds_name}_exists")
+    print(f"rm -rf {ds_name}")
+
+    print()
+
+print("-" * 80)
+
+for zip_path in paths:
+    ds_name = zip_path.stem
+    ds_path = zip_path.parent / ds_name
+
+    print(f"7z x {ds_path}.zip > /dev/null && touch {ds_name}_exists")
